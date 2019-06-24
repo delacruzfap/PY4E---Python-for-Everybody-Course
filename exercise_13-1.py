1e31960dfc8e211e8bc91e52ae47423d5a1df3c5
delacruzfap <36039457+delacruzfap@users.noreply.github.com>
2019-06-24 23:10:54 +0800

    Add files via upload

---
+++ exercise_13-1.py
@@ -0,0 +1,28 @@
+import urllib.request, urllib.parse, urllib.error
+import ssl
+import xml.etree.ElementTree as ET
+
+# Ignore certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location: ')
+# sample data = http://py4e-data.dr-chuck.net/comments_42.xml
+# actual data = http://py4e-data.dr-chuck.net/comments_245543.xml
+
+html = urllib.request.urlopen(url, context=ctx).read()
+tree = ET.fromstring(html)
+lst = tree.findall('comments/comment')
+
+print('Retrieving', url)
+counter = 0
+count_list = list()
+for item in lst:
+    counter = counter + 1
+    count = int(item.find('count').text)
+    count_list.append(count)
+
+print('Retrieved', )
+print('Count:', counter)
+print('Sum:', sum(count_list))
